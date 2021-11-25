876d3bf856031582f6d40f9da5415fa97e92b56d
Collin McNeese <cmcneese@chef.io>
2021-11-25 08:54:55 -0600

    fixes log level
    Signed-off-by: Collin McNeese <cmcneese@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Collin McNeese'
 maintainer_email 'cmcneese@chef.io'
 license 'Apache-2.0'
 description 'Installs/Configures log_hello'
-version '0.1.1'
+version '0.1.2'
 chef_version '>= 15.0'
 
 issues_url 'https://github.com/collinmcneese/log_hello/issues'
